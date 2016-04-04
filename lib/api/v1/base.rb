6b42de4ccb54d6d9fff7948c59a52937a8cb90f8
Fabiano Arruda <fabianoarruda@gmail.com>
2016-04-03 22:48:11 -0300

    Error handling for the API

--- lib/api/v1/base.rb
+++ lib/api/v1/base.rb
@@ -6,6 +6,22 @@ module Api
       version 'v1'
       format :json
 
+      rescue_from ActiveRecord::RecordNotFound do
+        rack_response({ 'message' => '404 Not found' }.to_json, 404)
+      end
+
+      rescue_from :all do |exception|
+
+        trace = exception.backtrace
+
+        message = "\n#{exception.class} (#{exception.message}):\n"
+        message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
+        message << "  " << trace.join("\n  ")
+
+        Base.logger.add Logger::FATAL, message
+        rack_response({ 'message' => '500 Internal Server Error' }.to_json, 500)
+      end
+
       mount Manufacturers
 
     end
