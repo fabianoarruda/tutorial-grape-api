8fd54fcd5f3120550256400f466a54b0170b5530
Fabiano Arruda <fabianoarruda@gmail.com>
2016-04-26 23:02:27 -0300

    PATCH and DELETE methods

--- lib/api/v1/vehicles.rb
+++ lib/api/v1/vehicles.rb
@@ -40,6 +40,39 @@ module Api
           end
         end
 
+        desc "Updates an existing vehicle"
+        params do
+          requires :id, type: Integer
+          optional :model, type: String
+          optional :description, type: String
+          optional :manufacturer_id, type: Integer
+          optional :image, type: Rack::Multipart::UploadedFile
+          optional :price, type: BigDecimal
+        end
+
+        patch ':id' do
+          if params[:image]
+            new_file = ActionDispatch::Http::UploadedFile.new(params[:image])
+            params.delete(:image)
+          end
+          vehicle = Vehicle.find(params[:id])
+          vehicle.assign_attributes(params)
+          vehicle.image = new_file if new_file
+
+          if vehicle.save
+            present vehicle, with: Entities::Vehicle
+          end
+        end
+
+        desc 'Delete a vehicle'
+        params do
+          requires :id, type: Integer, desc: 'Vehicle id.'
+        end
+        delete ':id' do
+          Vehicle.find(params[:id]).destroy
+          status :no_content
+        end
+
       end
     end
   end
